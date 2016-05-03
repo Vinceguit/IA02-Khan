7a3e9b0d67d6f0a34c1cf7cd93c2bade017e1258
Guillaume Hillion <glm.hillion@gmail.com>
2016-05-03 17:00:47 +0200

    Update main.pl

--- main.pl
+++ main.pl
@@ -1,1 +1,8 @@
 
+human(X):-write('You are human because you are '), write(X),
+write('Lets play Khan').
+
+%Création de la matrice des mouvements
+a = [[2,3,1,2,2,3], [2,1,3,1,3,1], [1,3,2,3,1,2], [3,1,2,1,3,2], [2,3,1,3,1,3], [2,1,3,2,2,1]].
+affiche([]).
+affiche([[D]|R]) :- write(D), nl,!, affiche(R).
