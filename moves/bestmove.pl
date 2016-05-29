0c904fbbc7a5789df0d0879be157c114b5056b8e
Vincent Baheux <vince.baheux@gmail.com>
2016-05-29 18:04:51 +0200

    Début du dev de la génération de mouvements

--- moves/bestmove.pl
+++ moves/bestmove.pl
@@ -4,4 +4,9 @@
 
 /*On appelle possibleMoves pour récupérer la liste des mouvements possibles.
 MoveList est sous la forme [[(X1, Y1),(X2, Y2)],...] où X est la colonne, Y est la ligne, (X1,Y1) est la position de départ et (X2, Y2) la position d'arrivée*/
-generateMove(Board, Player, Move) :- possibleMoves(Board, Player, MoveList).
+generateMove(Board, Player, Move) :- possibleMoves(Board, Player, MoveList),
+                                     findBestMove(MoveList, Move).
+
+/*MOUVEMENTS A ELIMINER
+1. Mouvement mettant en danger la Kalista*/
+findBestMove(_, _).
