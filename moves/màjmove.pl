c23b0b4ca239668cb92d0ed86872c0f92bc6259f
Guillaume Hillion <glm.hillion@gmail.com>
2016-05-30 19:20:37 +0200

    Update màjmove.pl

--- moves/màjmove.pl
+++ moves/màjmove.pl
@@ -1,3 +1,10 @@
+:-dynamic(piece/5).
+
+piece('r',1,2,'in',3).
+piece('o',3,5,'in',2).
+piece('r',2,2,'in',1).
+piece('o',5,3,'in',2).
+
 transfert(Board,Lin1,Col1,Lin2,Col2,Marq) :-  retract(piece(A,Lin1,Col1,B,C)), boardMarqueur(Board, Lin2,Col2,M), transfert(Board,Lin2,Col2,Lin2,Col2,Marq), asserta(piece(A,Lin2,Col2,B,M)),!.
 /* commande de test([[(2, b),(3, b)],[(2, b),(1, b)]],2,2,1,1,M).*/
 boardMarqueur([T|_], 1, Col,M) :- boardMarqueurDansLigne(T, Col,M).
