770a0419dc8597964f75b4ff460ba7cd11adbd2d
Guillaume Hillion <glm.hillion@gmail.com>
2016-05-30 17:56:46 +0200

    Update màjmove.pl

--- moves/màjmove.pl
+++ moves/màjmove.pl
@@ -1,5 +1,5 @@
 transfert(Board,Lin1,Col1,Lin2,Col2,Marq) :-  retract(piece(A,Lin1,Col1,B,C)), boardMarqueur(Board, Lin2,Col2,M), transfert(Board,Lin2,Col2,Lin2,Col2,Marq), asserta(piece(A,Lin2,Col2,B,M)),!.
-trransfert([[(2, b),(3, b)],[(2, b),(1, b)]],2,2,1,1,M).
+/* commande de test([[(2, b),(3, b)],[(2, b),(1, b)]],2,2,1,1,M).*/
 boardMarqueur([T|_], 1, Col,M) :- boardMarqueurDansLigne(T, Col,M).
 boardMarqueur([_|Q], Lin, Col, M) :- NLin is Lin-1, boardMarqueur(Q, NLin, Col,M).
 /*On trouve la colonne*/
