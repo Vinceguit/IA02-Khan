f84822080eea0e877a3fae97016485f423cd2fa4
Guillaume Hillion <Guillaume Hillion>
2016-06-06 22:20:42 +0200

    Finalisation du module legalMoves.pl - Reste à l'intégrer au reste du programme

--- moves/legalMoves.pl
+++ moves/legalMoves.pl
@@ -1,91 +1,119 @@
 
 
-activable(X,Y,Equipe,Marqueur) :- membreEquipe(Equipe,X,Y),
-findColour(TypePion, Equipe),
-									 pion(TypePion,X,Y,Ok,Marqueur),
-									 element(Ok,['in','khan']).
+activable(X,Y,Equipe,Marqueur) :- pion(TypePion,X,Y,Ok,Marqueur),
+								  findColour(TypePion, Equipe),
+								  element(Ok,['in','khan']).
 									 
-etablirEquipeActive(Equipe,Marqueur) :- setof((Col,Lin),activable(Col,Lin,Equipe,Marqueur),listeActivable),
-										recherchePionParPion(listeActivable,[],PossibleMoveList,Marqueur,Equipe),
-                                        demandeChoixMove(PossibleMoveList).
+etablirEquipeActive(Equipe,Marqueur) :- findall((Col,Lin),activable(Col,Lin,Equipe,Marqueur),ListeActivable),print(ListeActivable),
+										recherchePionParPion(ListeActivable,Marqueur,[],Equipe).
+    
 
-recherchePionParPion([(Col,Lin)|Q],I,L1,L3,Equipe) :- exploGraphe(Col,Lin,I,L1,L2,Equipe),
-													recherchePionParPion(Q,I,L2,L3,Equipe).
-recherchePionParPion([],_,_,_,_).
+recherchePionParPion([(ColInit,LinInit)|Q],I,L1,Equipe) :- exploGraphe(I,L1,L2,Equipe,ColInit,LinInit,ColInit,LinInit),nl,
+																
+														   recherchePionParPion(Q,I,L2,Equipe).
+recherchePionParPion([],_,PossibleMoveList,_):- demandeChoixMove(PossibleMoveList).
+
+exploGraphe(I,L1,L5,Equipe,ColInit,LinInit,Col,Lin) :- dir(gauche,I,L1,L2,Equipe,ColInit,LinInit,Col,Lin),
+													   dir(droite,I,L2,L3,Equipe,ColInit,LinInit,Col,Lin),
+													   dir(bas,I,L3,L4,Equipe,ColInit,LinInit,Col,Lin),
+													   dir(haut,I,L4,L5,Equipe,ColInit,LinInit,Col,Lin),!.
+													   
 
 exploGrapheVersGauche(I,L1,L4,Equipe,ColInit,LinInit,Col,Lin) :- dir(gauche,I,L1,L2,Equipe,ColInit,LinInit,Col,Lin),
 																 dir(bas,I,L2,L3,Equipe,ColInit,LinInit,Col,Lin),
-																 dir(haut,I,L3,L4,Equipe,ColInit,LinInit,Col,Lin).
+																 dir(haut,I,L3,L4,Equipe,ColInit,LinInit,Col,Lin),!.
 								  
-exploGrapheVersGauche(_,_,0,_,_,_).
+exploGrapheVersGauche(0,_,_,_,_,_,_,_).
 
 exploGrapheVersDroite(I,L1,L4,Equipe,ColInit,LinInit,Col,Lin) :- dir(droite,I,L1,L2,Equipe,ColInit,LinInit,Col,Lin),
 																 dir(bas,I,L2,L3,Equipe,ColInit,LinInit,Col,Lin),
-																 dir(haut,I,L3,L4,Equipe,ColInit,LinInit,Col,Lin).
+																 dir(haut,I,L3,L4,Equipe,ColInit,LinInit,Col,Lin),!.
 								  
-exploGrapheVersDroite(_,_,0,_,_,_).
+exploGrapheVersDroite(0,_,_,_,_,_,_,_).
 
 exploGrapheVersHaut(I,L1,L4,Equipe,ColInit,LinInit,Col,Lin) :- dir(gauche,I,L1,L2,Equipe,ColInit,LinInit,Col,Lin),
 													   dir(droite,I,L2,L3,Equipe,ColInit,LinInit,Col,Lin),
 													   
-													   dir(haut,I,L3,L4,Equipe,ColInit,LinInit,Col,Lin).
+													   dir(haut,I,L3,L4,Equipe,ColInit,LinInit,Col,Lin),!.
 								  
-exploGrapheVersHaut(_,_,0,_,_,_).
+exploGrapheVersHaut(0,_,_,_,_,_,_,_).
 
 exploGrapheVersBas(I,L1,L4,Equipe,ColInit,LinInit,Col,Lin) :- dir(gauche,I,L1,L2,Equipe,ColInit,LinInit,Col,Lin),
 													   dir(droite,I,L2,L3,Equipe,ColInit,LinInit,Col,Lin),
-													   dir(bas,I,L3,L4,Equipe,ColInit,LinInit,Col,Lin).
+													   dir(bas,I,L3,L4,Equipe,ColInit,LinInit,Col,Lin),!.
 								  
-exploGrapheVersBas(_,_,0,_,_,_).
+exploGrapheVersBas(0,_,_,_,_,_,_,_).
 								  
-dir(gauche,I,L1,L2,Equipe,ColInit,LinInit,Col,Lin) :- I>1,
+dir(gauche,I,L1,L2,Equipe,ColInit,LinInit,Col,Lin) :- write(I),write('gauche'),I>1,
 													  NewCol is Col-1,
 													  NewCol>0,
 													  caseVide(NewCol,Lin,I,Equipe),
-													  exploGrapheVersGauche(I,L1,L2,Equipe,ColInit,LinInit,NewCol,Lin).
+													  NewI is I-1,
+													  
+													  exploGrapheVersGauche(NewI,L1,L2,Equipe,ColInit,LinInit,NewCol,Lin),!.
 													  
 dir(gauche,1,L1,L2,Equipe,ColInit,LinInit,Col,Lin) :- NewCol is Col-1,
 													  NewCol>0,
 													  caseVide(NewCol,Lin,1,Equipe),
-													  append((ColInit,LinInit,NewCol,Lin),L1,L2).	
+													  write((ColInit,LinInit,NewCol,Lin)),
+													  append([(ColInit,LinInit,NewCol,Lin)],L1,L2),!.	
+dir(gauche,_,L,L,_,_,_,_,_).
 
-dir(droite,I,L1,L2,Equipe,ColInit,LinInit,Col,Lin) :- I>1,
+dir(droite,I,L1,L2,Equipe,ColInit,LinInit,Col,Lin) :- write(I),write('droite'),I>1,
 													  NewCol is Col+1,
 													  NewCol<7,
 													  caseVide(NewCol,Lin,I,Equipe),
-													  exploGrapheVersDroite(I,L1,L2,Equipe,ColInit,LinInit,NewCol,Lin).
+													  NewI is I-1,
+													  exploGrapheVersDroite(NewI,L1,L2,Equipe,ColInit,LinInit,NewCol,Lin),!.
+
+
 													  
-dir(droite,1,L1,L2,Equipe,ColInit,LinInit,Col,Lin) :- NewCol is Col-1,
+dir(droite,1,L1,L2,Equipe,ColInit,LinInit,Col,Lin) :- NewCol is Col+1,
 													  NewCol<7,
 													  caseVide(NewCol,Lin,1,Equipe),
+													  write((ColInit,LinInit,NewCol,Lin)),
+													  append([(ColInit,LinInit,NewCol,Lin)],L1,L2),!.	
 													  
-													  append((ColInit,LinInit,NewCol,Lin),L1,L2).	
+dir(droite,_,L,L,_,_,_,_,_).
 
-dir(haut,I,L1,L2,Equipe,ColInit,LinInit,Col,Lin) :- I>1,
+dir(haut,I,L1,L2,Equipe,ColInit,LinInit,Col,Lin) :- write(I),write('haut'),I>1,
 													NewLin is Lin-1,
 													NewLin>0,
 													caseVide(Col,NewLin,I,Equipe),
-													exploGrapheVersHaut(I,L1,L2,Equipe,ColInit,LinInit,Col,NewLin).
+													NewI is I-1,
+													
+													exploGrapheVersHaut(NewI,L1,L2,Equipe,ColInit,LinInit,Col,NewLin),!.
 													  
 dir(haut,1,L1,L2,Equipe,ColInit,LinInit,Col,Lin) :- NewLin is Lin-1,
 											        NewLin>0,
 													caseVide(Col,NewLin,1,Equipe),
-													append((ColInit,LinInit,Col,NewLin),L1,L2).												  
+													write((ColInit,LinInit,Col,NewLin)),
+													append([(ColInit,LinInit,Col,NewLin)],L1,L2),!.	
+dir(haut,_,L,L,_,_,_,_,_).													
 
-dir(bas,I,L1,L2,Equipe,ColInit,LinInit,Col,Lin) :-    I>1,
+dir(bas,I,L1,L2,Equipe,ColInit,LinInit,Col,Lin) :-    write(I),write('bas'),I>1,
 												      NewLin is Lin+1,
 													  NewLin<7,
 													  caseVide(Col,NewLin,I,Equipe),
-													  exploGrapheVersBas(I,L1,L2,Equipe,ColInit,LinInit,Col,NewLin).
+													  NewI is I-1,
+													  
+													  exploGrapheVersBas(NewI,L1,L2,Equipe,ColInit,LinInit,Col,NewLin),!.
 													  
 dir(bas,1,L1,L2,Equipe,ColInit,LinInit,Col,Lin) :- 
 													  NewLin is Lin+1,
 													  NewLin<7,
 													  caseVide(Col,NewLin,1,Equipe),
-													  append((ColInit,LinInit,Col,NewLin),L1,L2).
+													  write((ColInit,LinInit,Col,NewLin)),
+													  append([(ColInit,LinInit,Col,NewLin)],L1,L2),!.
 													  
-caseVide(X,Y,I,_) :- I>1, not(pion(_,X,Y,Ok,_)),element(Ok,['in','khan']).
-caseVide(X,Y,1,Equipe) :- not(pion(Equipe,X,Y,Ok,_)), element(Equipe,'rouge'), element(Ok,['in','khan']).
+dir(bas,_,L,L,_,_,_,_,_).
+												
 
-demandeChoixMove(PossibleMoveList) :- write('Quel mouvement wesh ?'),read(Move), element(Move,PossibleMoveList),write('ok'),!.
+
+											  
+caseVide(X,Y,I,_) :- I>1, \+ pion(_,X,Y,in,_),\+ pion(_,X,Y,khan,_).
+caseVide(X,Y,1,_) :- \+ pion(_,X,Y,in,_).
+caseVide(X,Y,1,Equipe) :- pion(TypePion,X,Y,in,_), \+ findColour(TypePion,Equipe).
+
+demandeChoixMove(PossibleMoveList) :- nl,affiche(PossibleMoveList),write('Quel mouvement wesh ?'),read(Move), element(Move,PossibleMoveList),write('ok'),!.
 demandeChoixMove(PossibleMoveList) :- demandeChoixMove(PossibleMoveList).
