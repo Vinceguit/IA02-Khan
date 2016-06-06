a1906eda48edaabeb31a4ba0aed5ef51310fc333
Guillaume Hillion <Guillaume Hillion>
2016-06-06 23:57:07 +0200

    Commentaire du code moves.pl et legalMoves.pl

--- moves/legalMoves.pl
+++ moves/legalMoves.pl
@@ -1,32 +1,38 @@
-
+/*possibleMoves permet d'obtenir le listing de tous les mouvements autorisés de la forme (Coldep,Lindep,Colarrivée,Linarrivée)*/
 possibleMoves(Player,PossibleMoveList):- pion(_,_,_,'khan',Marqueur),
 											   etablirEquipeActive(Player,Marqueur,PossibleMoveList).
 
 
-
+/* Une pièce est activable si elle appartient à l'equipe, si elle est en jeu et si son marqueur est le même que celui du Khan*/
 activable(X,Y,Equipe,Marqueur) :- pion(TypePion,X,Y,Ok,Marqueur),
 								  findColour(TypePion, Equipe),
 								  element(Ok,['in','khan']).
-									 
+
+/*Renvoie l'ensemble des pions jouables*/								  
 etablirEquipeActive(Equipe,Marqueur,PossibleMoveList) :- findall((Col,Lin),activable(Col,Lin,Equipe,Marqueur),ListeActivable),print(ListeActivable),
 										recherchePionParPion(ListeActivable,Marqueur,[],Equipe,PossibleMoveList).
     
-
+/*Pour chaque pion jouable, une recherche de mouvement est lancée grâce au exploGraphe*/
 recherchePionParPion([(ColInit,LinInit)|Q],I,L1,Equipe,PossibleMoveList) :- exploGraphe(I,L1,L2,Equipe,ColInit,LinInit,ColInit,LinInit),nl,
 																
 														   recherchePionParPion(Q,I,L2,Equipe,PossibleMoveList).
+/* Une fois que tousles pions sont testés, on récupère la PossibleMoveList, sans doublons*/
 recherchePionParPion([],_,PossibleMoveList,_,PossibleMoveList).
 
+/*Pour la première recherche, l'exploration se fait dans toutes les directions*/
 exploGraphe(I,L1,L5,Equipe,ColInit,LinInit,Col,Lin) :- dir(gauche,I,L1,L2,Equipe,ColInit,LinInit,Col,Lin),
 													   dir(droite,I,L2,L3,Equipe,ColInit,LinInit,Col,Lin),
 													   dir(bas,I,L3,L4,Equipe,ColInit,LinInit,Col,Lin),
 													   dir(haut,I,L4,L5,Equipe,ColInit,LinInit,Col,Lin),!.
 													   
-
+/* Si le mouvement est vers la gauche, on lance exploGrapheVersGauche, l'un des 4 exploGrapheVers_
+Chaque prédicat exploGrapheVers_ interdit à la recherche de retourner en arrière 
+pas de dir(droite,_,_,_,_,_,_,_) dans exploGrapheVersGauche)
+L'exploration se fait sur une distance de Manhattan de I */
 exploGrapheVersGauche(I,L1,L4,Equipe,ColInit,LinInit,Col,Lin) :- dir(gauche,I,L1,L2,Equipe,ColInit,LinInit,Col,Lin),
 																 dir(bas,I,L2,L3,Equipe,ColInit,LinInit,Col,Lin),
 																 dir(haut,I,L3,L4,Equipe,ColInit,LinInit,Col,Lin),!.
-								  
+/* Une fois que la pièce a fait I mouvements, l'exploration est stoppée*/								  
 exploGrapheVersGauche(0,_,_,_,_,_,_,_).
 
 exploGrapheVersDroite(I,L1,L4,Equipe,ColInit,LinInit,Col,Lin) :- dir(droite,I,L1,L2,Equipe,ColInit,LinInit,Col,Lin),
@@ -47,6 +53,9 @@ exploGrapheVersBas(I,L1,L4,Equipe,ColInit,LinInit,Col,Lin) :- dir(gauche,I,L1,L2
 													   dir(bas,I,L3,L4,Equipe,ColInit,LinInit,Col,Lin),!.
 								  
 exploGrapheVersBas(0,_,_,_,_,_,_,_).
+
+/* Pour I>1, on ne peut pas manger de pièce, on doit donc continuer l'exploration du plateau si la case est vide
+Il est nécéssaire de vérifier que la pièce ne sort pas du plateau de jeu*/
 								  
 dir(gauche,I,L1,L2,Equipe,ColInit,LinInit,Col,Lin) :- write(I),write('gauche'),I>1,
 													  NewCol is Col-1,
@@ -56,11 +65,16 @@ dir(gauche,I,L1,L2,Equipe,ColInit,LinInit,Col,Lin) :- write(I),write('gauche'),I
 													  
 													  exploGrapheVersGauche(NewI,L1,L2,Equipe,ColInit,LinInit,NewCol,Lin),!.
 													  
+/*Pour I=1, il est désormais possible de manger une pièce adverse
+Dès Lors si la case est vide ou contient un ennemi, le mouvement est accepté dans L2
+L2 est est une sous-partie de PossibleMoveList*/
 dir(gauche,1,L1,L2,Equipe,ColInit,LinInit,Col,Lin) :- NewCol is Col-1,
 													  NewCol>0,
 													  caseVide(NewCol,Lin,1,Equipe),
 													  write((ColInit,LinInit,NewCol,Lin)),
-													  append([(ColInit,LinInit,NewCol,Lin)],L1,L2),!.	
+													  append([(ColInit,LinInit,NewCol,Lin)],L1,L2),!.
+/*Si la case est bloquée, pas de modification dans la liste d'entrée L
+donc la liste de sortie de cette branche de l'exploration est aussi L */ 													  
 dir(gauche,_,L,L,_,_,_,_,_).
 
 dir(droite,I,L1,L2,Equipe,ColInit,LinInit,Col,Lin) :- write(I),write('droite'),I>1,
@@ -113,7 +127,8 @@ dir(bas,1,L1,L2,Equipe,ColInit,LinInit,Col,Lin) :-
 dir(bas,_,L,L,_,_,_,_,_).
 												
 
-
+/*caseVide teste uniquement si la case est vide pour I>1
+Une fois arrivé à I=1, au dernier mouvement, caseVide est vraie si la case est vide ou occupée par un ennemi */
 											  
 caseVide(X,Y,I,_) :- I>1, \+ pion(_,X,Y,in,_),\+ pion(_,X,Y,khan,_).
 caseVide(X,Y,1,_) :- \+ pion(_,X,Y,in,_).
--- moves/moves.pl
+++ moves/moves.pl
@@ -7,15 +7,30 @@
 /*transfert([[(2, b),(3, r1)],[(2, b),(1, o3)]],(1,1,2,1),NewBoard) --> [[(2,b),(3,r1)],[(2,r1),(1,o3)]].*/
 /*Prédicat de pion : pion(IdPion, Col, Lin, in, IdCase)/
 
-/*Procedure de mise à jour de tous les effets d'un mouvement sur le plateau et sur les pieces sur la BDD du jeu*/
+/*Procedure de mise à jour de tous les effets d'un mouvement sur le plateau et sur les pieces sur la BDD du jeu
+
+Cas 1: Le mouvement saisi fait arriver la pièce sur une case occupée par une pièce ennemie*/
+
 transfert(InBoard,Move,OutBoard) :- presenceProie(Move ,InBoard, NewBoard), !,
+									/* On cherche le nouveau marqueur (1,2 ou 3) associé à la position d'arrivée*/
                                     rechercheMarqueur(NewBoard, Move, NewMarqueur),
+									/*On vient faire toutes les modifications pour modifier les infos dans la BDD*/
                                     enregistrementMove(Move, NewMarqueur, NewBoard, OutBoard),
                                     print('Pièce adverse capturée !'), nl.
+
+/* Cas 2: La case d'arrivée est vide*/
 
 transfert(InBoard,Move,OutBoard) :- rechercheMarqueur(InBoard, Move, NewMarqueur),
                                     enregistrementMove(Move, NewMarqueur, InBoard, OutBoard).
 
+/* La pièce bougée change de position et devient le khan*/
+enregistrementMove((Col1, Lin1, Col2, Lin2), NewMarqueur, Board1, Board2) :- pion(TypePion, Col1, Lin1, 'in', M),
+                                                                             retract(pion(TypePion, _, _,_,M)),
+                                                                             asserta(pion(TypePion, Col2, Lin2, 'khan', NewMarqueur)),
+                                                                             miseAJourMove(TypePion, Col1, Lin1, Col2, Lin2, 'in', Board1, Board2).
+
+
+/* Suppression de la pièce présente au point de chute de la pièce bougée*/									
 presenceProie((_, _, Col2, Lin2), Board, NewBoard) :- pion(TypePion, Col2, Lin2, 'in', _),
                                                             suppressionProie(TypePion, Col2, Lin2),
                                                             miseAJourPlateau(TypePion, Col2, Lin2, 'out', Board, NewBoard).
@@ -24,16 +39,13 @@ presenceProie((_, _, Col2, Lin2), Board, NewBoard) :- pion(TypePion, Col2, Lin2,
                                                             suppressionProie(TypePion, Col2, Lin2),
                                                             miseAJourPlateau(TypePion, Col2, Lin2, 'out', Board, NewBoard).
 
+/*Passage de la pièce de in à out*/															
 suppressionProie(TypePion,Col,Lin) :- retract(pion(TypePion, Col, Lin, _, _)),
                                       asserta(pion(TypePion,0,0, 'out', 0)).
 
-enregistrementMove((Col1, Lin1, Col2, Lin2), NewMarqueur, Board1, Board2) :- pion(TypePion, Col1, Lin1, 'in', M),
-                                                                             retract(pion(TypePion, _, _,_,M)),
-                                                                             asserta(pion(TypePion, Col2, Lin2, 'khan', NewMarqueur)),
-                                                                             miseAJourMove(TypePion, Col1, Lin1, Col2, Lin2, 'in', Board1, Board2).
 
 
-
+/* Remplacement des pièces sur le plateau de jeu ASCII
 miseAJourMove(TypePion, Col1,Lin1, Col2, Lin2, 'in', Board1, Board3) :- miseAJourPlateau(TypePion, Col1, Lin1, 'out', Board1, Board2),
                                                                         miseAJourPlateau(TypePion, Col2, Lin2, 'in', Board2, Board3).
 
