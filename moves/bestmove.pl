ffe8f751d71c0f5a34ce3ef5e889bb1ff44cbe2c
Vincent Baheux <vince.baheux@gmail.com>
2016-06-09 23:55:33 +0200

    Fix d'erreur de nom de variables dans Alpha Beta

--- moves/bestmove.pl
+++ moves/bestmove.pl
@@ -11,12 +11,12 @@ generateMove(Board, Player, Move) :- alphaBeta(Player, 4, Board, -100, 100, Move
 /*alphaBeta(Player, Frontier, Board, Alpha, Beta, Move, Value) où Value est l'heuristique*/
 
 /*Cas 1 : si la frontière est à 0, on arrête l'exploration et on calcule l'heuristique*/
-alphaBeta(Player, 0, Board, _, _, _, Value) :-
+alphaBeta(_, 0, Board, _, _, _, Value) :-
   heuristic(Board, Value).
 
 /*Cas 2 : On récupère les mouvements possibles pour le joueur, puis on évalue les mouvements possibles*/
 alphaBeta(Player, Frontier, Board, Alpha, Beta, Move, Value) :-
-  Frontiere > 0, % On vérifie que la frontière est bien positive !
+  Frontier > 0, % On vérifie que la frontière est bien positive !
   possibleMoves(Board, Player, MoveList), % On récupère les mouvements possibles
   Alpha1 is -Beta, % On change de joueur; on passe donc de MAX à MIN ou vice-versa.
   Beta1 is -Alpha,
@@ -30,7 +30,7 @@ evaluerEtChoisir(Player, [Move|MoveList], Board, Frontier, Alpha, Beta, Record, 
   oppPlayer(Player, OtherPlayer), % On change de joueur (pour minimiser les chances de l'adversaire / maximiser celles de l'IA)
   alphaBeta(OtherPlayer, Frontier, OutBoard, Alpha, Beta, _, Value), % On rappelle alphaBeta à la profondeur suivante. _ : OtherMove
   NValue is -Value,
-  coupure(Player, Move, NValue, Frontier, Alpha, Beta, MoveList, Position, Record, BestMove).
+  coupure(Player, Move, NValue, Frontier, Alpha, Beta, MoveList, Board, Record, BestMove).
 
 evaluerEtChoisir(_, [], _, _, Alpha, _, Move, (Move, Alpha)). %Si on n'a plus de mouvement à traiter dans la liste, on renvoie la valeur de Alpha ainsi que le mouvement.
 
@@ -39,13 +39,13 @@ evaluerEtChoisir(_, [], _, _, Alpha, _, Move, (Move, Alpha)). %Si on n'a plus de
 coupure(_, Move, Value, _, _, Beta, _, _, _, (Move, Value)) :-
   Value >= Beta, !.
 
-coupure(Player, Move, Value, Frontier, Alpha, Beta, Moves, Position, _, BestMove) :-
+coupure(Player, Move, Value, Frontier, Alpha, Beta, Moves, Board, _, BestMove) :-
   Alpha < Value, Value < Beta, !,
-  evaluerEtChoisir(Player, Moves, Position, Frontier, Value, Beta, Move, BestMove).
+  evaluerEtChoisir(Player, Moves, Board, Frontier, Value, Beta, Move, BestMove).
 
-coupure(Player, _, Value, Frontier, Alpha, Beta, Moves, Position, Record, BestMove) :-
+coupure(Player, _, Value, Frontier, Alpha, Beta, Moves, Board, Record, BestMove) :-
   Value =< Alpha, !,
-  evaluerEtChoisir(Player, Moves, Position, Frontier, Alpha, Beta, Record, BestMove).
+  evaluerEtChoisir(Player, Moves, Board, Frontier, Alpha, Beta, Record, BestMove).
 
 
 /*Petit prédicat pour trouver le joueur adverse*/
